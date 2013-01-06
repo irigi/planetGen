907a1635a117a98eab5d846d52aaa1cb03c2af48
irigi <irigi@matfyz.cz>
2013-01-06 23:32:51 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// Copyright © 2008-2013 planetGen Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#include <stdio.h>
+#include <math.h>
+#include "Surface.h"
+
+int main(int argc, char* argv[]) {
+	printf("Under von Construction\n");
+}
