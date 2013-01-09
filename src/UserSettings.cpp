a66075c6a9f589d410bf63d77f89343ddffc0523
irigi <irigi@matfyz.cz>
2013-01-09 17:13:51 +0100

    User settings

---
+++ src/UserSettings.cpp
@@ -0,0 +1,7 @@
+// Copyright © 2008-2013 planetGen Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#include <stdio.h>
+#include "UserSettings.h"
+
+int Settings::TectonicPlates = 7;
---
+++ src/UserSettings.h
@@ -0,0 +1,7 @@
+// Copyright © 2008-2013 planetGen Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+class Settings {
+public:
+	static int TectonicPlates;
+};
